c71af0547896ed050fcc583d57c23c7c79bb6fde
seokhyun02 <seokhyunh320@gmail.com>
2024-09-11 19:45:08 -0400

    SH Push test 2

--- Capstone/test/widget_test.dart
+++ Capstone/test/widget_test.dart
@@ -10,7 +10,7 @@ import 'package:flutter_test/flutter_test.dart';
 
 import 'package:capstone/main.dart';
 
-// SH
+// SH2
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
